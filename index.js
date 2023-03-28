7871ab73f202770648ef33bdcda8492d09afeaf8
Benah0987 <wanyoikebenayah77@gmail.com>
2023-03-28 13:32:12 +0300

    Update index.js
    added functions to the index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+//a function called divide
+function divide() {
+
+    return 2000 / 100;
+  }
+ console.log(divide()); 
+
+//a function called square has a parameter x and uses the arrow function  
+  const square = (x) => {
+    return x * x;
+  }
+  console.log(square(2));
+
+  // function add, two parameters, arrow funtion
+  const add = (a, b) => {
+    return a + b;
+  }
+  console.log(add(3,4));
+  
+ 
+  
